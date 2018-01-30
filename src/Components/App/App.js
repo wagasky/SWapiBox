0dc4cd1e7920fafceccf69695816209f7beb48c2
Melissa Wagasky <melissawagasky@gmail.com>
2018-01-30 14:39:59 -0700

    Add components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+
+  }
+  
   render() {
     return (
       <div className="App">
